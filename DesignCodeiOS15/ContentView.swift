269f523737c6e29d44dbc05443562d3f38fc797d
Steffen Suess <steffen@steffensuess.com>
2021-12-01 10:14:07 +0100

    initial commit

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(alignment: .leading, spacing: 8.0) {
+            Spacer()
+            Image("Logo 2")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 26.0, height: 26.0)
+                .cornerRadius(20.0)
+            Text("SwiftUI for iOS15")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
+                .font(.footnote)
+                .fontWeight(.semibold)
+                .foregroundColor(.secondary)
+            Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                .font(.footnote)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .foregroundColor(.secondary)
+        }
+        .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+        .padding(.vertical, 20)
+        .frame(height: 350.0)
+        .background(Color("Background"))
+        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .padding(.horizontal, 20)
+            
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .previewDevice("iPhone 13 Pro Max")
+            ContentView()
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+                .previewDevice("iPhone 13 Pro Max")
+        }
     }
 }
