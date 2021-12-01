6e77d570b46ba3a9f1d6acac38e17df064065880
Steffen Suess <steffen@steffensuess.com>
2021-12-01 08:38:21 +0100

    Initial Commit

---
+++ DesignCodeiOS15/DesignCodeiOS15App.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeiOS15App.swift
+//  DesignCodeiOS15
+//
+//  Created by Steffen Suess on 01.12.21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeiOS15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
