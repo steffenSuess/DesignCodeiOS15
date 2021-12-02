6f0dc2f085b5f27d1192ad135c19b2d6daa302f1
Steffen Suess <steffen@steffensuess.com>
2021-12-02 11:45:34 +0100

    shapes, strokes and blends

--- DesignCodeiOS15/ContentView.swift
+++ DesignCodeiOS15/ContentView.swift
@@ -16,13 +16,16 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20.0)
+                .padding(9)
+                .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
             Text("SwiftUI for iOS15")
                 .font(.largeTitle)
                 .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary, .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
             Text(/*@START_MENU_TOKEN@*/"20 sections - 3 hours"/*@END_MENU_TOKEN@*/.uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
-                .foregroundColor(.secondary)
+                .foregroundStyle(.secondary)
             Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
@@ -33,11 +36,23 @@ struct ContentView: View {
         .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
         .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(Color("Background"))
-        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 30, style: .continuous))
+        //        .cornerRadius(/*@START_MENU_TOKEN@*/30.0/*@END_MENU_TOKEN@*/)
+        //        .mask(RoundedRectangle(cornerRadius: 30, style: .continuous))
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 0, y: 10)
+        .overlay(RoundedRectangle(cornerRadius: 30, style: .continuous)
+                    .stroke(.linearGradient(colors: [.white.opacity(0.3), .black.opacity(0.1)], startPoint: .top, endPoint: .bottom)).blendMode(.overlay))
         .padding(.horizontal, 20)
-            
+        .background(
+            Image("Blob 1").offset(x: 250, y: -100)
+        )
+        .overlay(
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80))
+        
     }
 }
 
@@ -48,7 +63,6 @@ struct ContentView_Previews: PreviewProvider {
                 .previewDevice("iPhone 13 Pro Max")
             ContentView()
                 .preferredColorScheme(.dark)
-                .environment(\.sizeCategory, .extraExtraExtraLarge)
                 .previewDevice("iPhone 13 Pro Max")
         }
     }
