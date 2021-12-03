cd15a30923ffb00cd68849517c068440ecf70505
Steffen Suess <steffen@steffensuess.com>
2021-12-03 09:43:07 +0100

    adds reusable styles

---
+++ DesignCodeiOS15/Styles.swift
@@ -0,0 +1,30 @@
+//
+//  Styles.swift
+//  DesignCodeiOS15
+//
+//  Created by Steffen Suess on 03.12.21.
+//
+
+import SwiftUI
+
+struct StrokeStyle: ViewModifier {
+    var cornerRadius: CGFloat
+    @Environment(\.colorScheme) var colorScheme
+    func body(content: Content) -> some View {
+        content.overlay(RoundedRectangle(cornerRadius: cornerRadius, style: .continuous)
+                            .stroke(
+                                .linearGradient(
+                                    colors: [
+                                        .white.opacity(colorScheme == .dark ? 0.6 : 0.3),
+                                        .black.opacity(colorScheme == .dark ? 0.3 : 0.1)
+                                    ], startPoint: .top, endPoint: .bottom
+                                )
+                            )
+                            .blendMode(.overlay))
+    }
+}
+extension View {
+    func strokeStyle(cornerRadius: CGFloat = 30) -> some View {
+        modifier(StrokeStyle(cornerRadius: cornerRadius))
+    }
+}
